f0b34446bc979bb50be17c8378f26309a1181da7
권해원 <qpinky12@naver.com>
2022-12-24 22:35:47 -0800

    FEAT:ITIT

---
+++ main.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const {app,BrowserWindow}=electron;
+
+let win;
+
+app.on("ready",()=>{
+    const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+    const options ={
+        width:width,
+        height:height,
+        resizable:false,
+        fullscreenable:false,
+        webPreferences:{
+            affinity:true,
+            nodeIntegration:false
+        }
+    };
+    win = new BrowserWindow(options);
+    win.loadURL(url.format({
+        pathname:path.join(__dirname, "login.html"),
+        protocol:"file"
+    }))
+});
+app.on("window-all-closed",()=>{
+    app.quit();
+});
+app.on("activate",()=>{
+    app.quit();
+});
